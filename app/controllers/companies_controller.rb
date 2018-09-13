c4d31b5e3f5f176555e60b3f26fbc590fc40fad0
Patrick Beeker <patrick.beeker@gmail.com>
2018-09-13 17:24:20 +0300

    Added basic Company controller and first actions

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,22 @@
+class CompaniesController < ApplicationController
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    @company.save
+    redirect_to @company
+  end
+
+  private
+    def company_params
+      params.require(:company).permit(:name, :mission)
+    end
+
+end
